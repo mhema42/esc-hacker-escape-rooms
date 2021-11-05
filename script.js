94bfced97ae7cbe5b1daebdcc5ec9bb42f929d7e
mhema42 <mhema@telia.com>
2021-11-05 14:59:33 +0100

    Hamburger done, emerges @ 640px and dow

---
+++ script.js
@@ -0,0 +1,5 @@
+
+document.getElementById("ham_btn").addEventListener("click", function() {
+    document.getElementById("nav_menu").style.position = "relative";
+    document.getElementById("nav_menu").style.display = "block";
+});
